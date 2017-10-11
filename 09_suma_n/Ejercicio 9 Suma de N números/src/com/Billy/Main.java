5a0bfa24b846e97618724c5b7832633d1a2d1855
Unknown <Eric.Mtz.DAM@gmail.com>
2017-10-11 21:41:06 +0200

    Ejercicio 9

---
+++ 09_suma_n/Ejercicio 9 Suma de N números/src/com/Billy/Main.java
@@ -0,0 +1,33 @@
+package com.Billy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int vueltas = 0;
+        double SumaTotal = 0;
+
+        System.out.println("                 Suma de N numeros");
+        System.out.println("          -------------------------------");
+
+        System.out.print("Introduzca la cantidad de numeros que desea sumar: ");
+        int VueltasUsuario = Integer.parseInt(br.readLine());
+
+        while (vueltas < VueltasUsuario){
+            System.out.print("Introduzaca un numero: ");
+            double a = Double.parseDouble(br.readLine());
+            SumaTotal = SumaTotal + a;
+            vueltas++;
+        }
+        System.out.println("          -------------------------------");
+        System.out.print("El resultado de la suma es: ");
+        System.out.print(SumaTotal);
+
+    }
+}
